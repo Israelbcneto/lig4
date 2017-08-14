f156f8d0b1747da62c109494cc32a1ce2ddae746
Israel Neto <israelbcneto@hotmail.com>
2017-08-14 10:23:53 -0300

    Update drk.py
    mudança

--- drk.py
+++ drk.py
@@ -1,6 +1,4 @@
 pos_preenchidas = []
-LINHA = int(input("Digite o numero desejado de linhas:"))
-COLUNA = int(input("Digite o numero desejado de colunas:"))
 PECA2 = " X "
 PECA1 = " O "
 VAZIA = " - "
@@ -24,32 +22,56 @@ def escolheColuna(player,ultPeca):
             player = PECA1
         if ultPeca == PECA2:
             player = PECA2
-    if player == PECA1:                     #Aqui entra quando o player é X, ou seja, na primeira rodada do jogo
+    if player == PECA1:                                                                    #Aqui entra quando o player é X, ou seja, na primeira rodada do jogo
             coluna = int(input("Player 1, escolha a coluna para seu próximo movimento: "))
-            player = PECA2                  #Aqui o player muda pra O
-            ultPeca = PECA1                 #Aqui é a variável que indica se o último jogador foi X ou O
-    else:                                   #Aqui entra quando o player é O, ou seja, na segunda rodada do jogo
+            player = PECA2                                                                 #Aqui o player muda pra O
+            ultPeca = PECA1                                                                #Aqui é a variável que indica se o último jogador foi X ou O
+    else:                                                                                  #Aqui entra quando o player é O, ou seja, na segunda rodada do jogo
             coluna = int(input("Player 2, escolha a coluna para seu próximo movimento: "))
-            player = PECA1                  #Aqui o player muda pra X
-            ultPeca = PECA2                 #Aqui é a variável que indica se o último jogador foi X ou O
+            player = PECA1                                                                 #Aqui o player muda pra X
+            ultPeca = PECA2                                                                #Aqui é a variável que indica se o último jogador foi X ou O
     return coluna, ultPeca, player
 
 def printar_matriz(LINHA, matriz):
     for i in range(LINHA):
         print(matriz[i])
 
-def jogar(matriz, pos_preenchidas,col):
+def jogar(matriz, pos_preenchidas, coluna):
     matriz = criarMatriz(matriz, linha)
     if pos_preenchidas <0:
-        print("Coluna cheia, escolha outra")
+        print("Coluna cheia, escolha outra.")
         voltar = True
-    elif matriz[pos_preenchidas][col-1]==VAZIA:
-        matriz[pos_preenchidas][col-1] = player
+    elif matriz[pos_preenchidas][coluna-1]==VAZIA:
+        matriz[pos_preenchidas][coluna - 1] = player
         printar_matriz(LINHA, matriz)
         voltar = False
         a = pos_preenchidas
         return a - 1,voltar
     return pos_preenchidas,voltar
+def definir_linha():
+    while(True):
+        try:
+            LINHA = int(input("Digite o numero desejado de linhas: "))
+            while (LINHA <1):
+                LINHA = int(input("Numero incorreto de linhas, escolha outro: "))
+            break
+        except ValueError:
+            print("Numero incorreto de linhas, escolha outro. ")
+    return LINHA
+
+def definir_coluna():
+    while(True):
+        try:
+            COLUNA = int(input("Digite o numero desejado de colunas: "))
+            while (COLUNA <1):
+                COLUNA = int(input("Numero incorreto de colunas, escolha outro: "))
+            break
+        except ValueError:
+                print("Numero incorreto de colunas, escolha outro. ")
+    return COLUNA
+
+COLUNA = definir_coluna()
+LINHA = definir_linha()
 
 for i in range(COLUNA):
     pos_preenchidas.append(LINHA-1)
